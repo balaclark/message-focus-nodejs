cf616cdfe708a1d39542c615c92fde6ca6d6b353
Bala Clark <balaclark@gmail.com>
2015-10-14 13:40:45 +0100

    adding tests for xmlrpc.createClient

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-var xmlrpc = require("xmlrpc");
+var xmlrpc = require('xmlrpc');
 
-var ENDPOINT = "https://app.adestra.com/api/xmlrpc";
+var ENDPOINT = 'https://app.adestra.com/api/xmlrpc';
 
 var MessageFocus = function(accountName, userName, password) {
   var client = null;
 
   this.methodCall = function(method, params, cb) {
-    if(client === null) {
+    if (client === null) {
       client = xmlrpc.createClient({url: ENDPOINT, basic_auth: {user: accountName + '.' + userName, pass: password}});
     }
-    console.log("DEBUG", method, params);
+    console.log('DEBUG', method, params);
     client.methodCall(method, params, cb);
   };
 
-  this.workspace = require("./lib/workspace")(this);
+  this.workspace = require('./lib/workspace')(this);
   this.project = require('./lib/project')(this);
   this.campaign = require('./lib/campaign')(this);
   this.contact = require('./lib/contact')(this);
---
+++ test/index.test.js
@@ -0,0 +1,51 @@
+var assert = require('assert-diff')
+  , sinon = require('sinon')
+  , rewire = require('rewire')
+  , MessageFocus = rewire('..')
+
+function XmlrpcSpy() {
+  this.methodCall = sinon.spy(function (method, params, cb) { cb() })
+  this.createClient = sinon.spy(function () {
+    return { methodCall: this.methodCall }
+  })
+}
+
+describe('MessageFocus', function () {
+  var xmlrpcSpy
+ 
+  beforeEach(function () {
+    xmlrpcSpy = new XmlrpcSpy() 
+    MessageFocus.__set__('xmlrpc', xmlrpcSpy) 
+  })
+
+  describe('methodCall', function () {
+
+    it('should setup an authed xmlrpc client', function (done) {
+      var mf = new MessageFocus('account', 'user', 'password')
+      mf.methodCall('test', null, function () {
+        assert(xmlrpcSpy.createClient.calledOnce)
+        assert.deepEqual(xmlrpcSpy.createClient.firstCall.args[0]
+        , { url: 'https://app.adestra.com/api/xmlrpc'
+          , basic_auth:
+            { user: 'account.user'
+            , pass: 'password'
+            }
+          })
+        done() 
+      })
+    })
+
+    it('the authed xmlrpc client should be memoized', function (done) {
+      var mf = new MessageFocus('account', 'user', 'password')
+      mf.methodCall('test', null, function () {
+        mf.methodCall('test', null, function () {
+          mf.methodCall('test', null, function () {
+            assert(xmlrpcSpy.createClient.calledOnce)
+            assert(xmlrpcSpy.methodCall.calledThrice)
+            done() 
+          })
+        })
+      })
+    })
+  })
+})
